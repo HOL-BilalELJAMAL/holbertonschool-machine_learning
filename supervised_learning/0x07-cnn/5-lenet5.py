40e7f5110fa0d4708e0d19945d1f70ae088de600
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-06-08 16:15:24 +0300

    Add Task 5

---
+++ supervised_learning/0x07-cnn/5-lenet5.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""
+5-lenet5.py
+Module that defines a function called lenet5
+"""
+
+import tensorflow.keras as K
+
+
+def lenet5(X):
+    """
+    Function that builds a modified version of the LeNet-5 architecture
+    using keras
+
+    Args:
+        X (K.Input): shape (m, 28, 28, 1) containing the input images for the
+                     network
+
+    Returns:
+        A K.Model compiled to use Adam optimization (with default
+        hyperparameters) and accuracy metrics
+    """
+    C1 = K.layers.Conv2D(filters=6,
+                         kernel_size=5,
+                         padding='same',
+                         activation='relu',
+                         kernel_initializer='he_normal')(X)
+
+    S2 = K.layers.MaxPooling2D(pool_size=(2, 2),
+                               strides=(2, 2))(C1)
+
+    C3 = K.layers.Conv2D(filters=16,
+                         kernel_size=5,
+                         padding='valid',
+                         activation='relu',
+                         kernel_initializer='he_normal')(S2)
+
+    S4 = K.layers.MaxPooling2D(pool_size=(2, 2),
+                               strides=(2, 2))(C3)
+
+    flatten = K.layers.Flatten()(S4)
+
+    C5 = K.layers.Dense(units=120,
+                        activation='relu',
+                        kernel_initializer='he_normal')(flatten)
+
+    F6 = K.layers.Dense(units=84,
+                        activation='relu',
+                        kernel_initializer='he_normal')(C5)
+
+    OUTPUT = K.layers.Dense(units=10,
+                            activation='softmax',
+                            kernel_initializer='he_normal')(F6)
+
+    model = K.Model(inputs=X, outputs=OUTPUT)
+
+    model.compile(optimizer=K.optimizers.Adam(),
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    return model
