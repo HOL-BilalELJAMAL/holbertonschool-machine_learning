93f01192931f33dbefb65ebbe8168a8bb1c7e486
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-06-09 12:07:07 +0300

    Add Task 2

--- supervised_learning/0x07-cnn/2-conv_backward.py
+++ supervised_learning/0x07-cnn/2-conv_backward.py
@@ -27,40 +27,37 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
         The partial derivatives with respect to the previous layer (dA_prev),
         the kernels (dW), and the biases (db), respectively
     """
-    m, h_new, w_new, c_new = dZ.shape
-    _, h_prev, w_prev, c_prev = A_prev.shape
-    kh, kw, _, _ = W.shape
-    sh, sw = stride
-
-    pw, ph = 0, 0
-
-    db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
-
-    if padding == 'same':
-        ph = int(np.ceil(((h_prev-1)*sh+kh-h_prev)/2))
-        pw = int(np.ceil(((w_prev-1)*sw+kw-w_prev)/2))
-
-    A_prev = np.pad(A_prev,
-                    pad_width=((0, 0),
-                               (ph, ph),
-                               (pw, pw),
-                               (0, 0)),
-                    mode='constant', constant_values=0)
-
-    dA = np.zeros(A_prev.shape)
+    hk = W.shape[0]
+    wk = W.shape[1]
+    nc = W.shape[3]
+    m = A_prev.shape[0]
+    hm = A_prev.shape[1]
+    wm = A_prev.shape[2]
+    st1 = stride[1]
+    st0 = stride[0]
+    if padding == "valid":
+        pad0 = 0
+        pad1 = 0
+    else:
+        pad0 = int(((hm - 1) * st0 + hk - hm) / 2) + 1
+        pad1 = int(((wm - 1) * st1 + wk - wm) / 2) + 1
+    out_h = int((hm + 2 * pad0 - hk) / st0) + 1
+    out_w = int((wm + 2 * pad1 - wk) / st1) + 1
+    x_pad = np.pad(A_prev, ((0, 0), (pad0, pad0), (
+                           pad1, pad1), (0, 0)), 'constant')
+    dX = np.zeros(x_pad.shape)
     dW = np.zeros(W.shape)
-
-    for z in range(m):
-        for y in range(h_new):
-            for x in range(w_new):
-                for k in range(c_new):
-                    aux_W = W[:, :, :, k]
-                    aux_dz = dZ[z, y, x, k]
-                    dA[z, y*sh: y*sh+kh, x*sw: x*sw+kw, :] += aux_dz * aux_W
-                    aux_A_prev = A_prev[z, y*sh: y*sh+kh, x*sw: x*sw+kw, :]
-                    dW[:, :, :, k] += aux_A_prev * aux_dz
-
-    dA = dA[:, ph:dA.shape[1] - ph, pw:dA.shape[2] - pw, :]
-
-    return dA, dW, db
-
+    db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
+    for i in range(m):
+        for h in range(out_h):
+            for w in range(out_w):
+                for c in range(nc):
+                    dX[i, h * st0: h * st0 + hk,
+                       w * st1: w * st1 + wk, :] += dZ[
+                       i, h, w, c] * W[:, :, :, c]
+                    dW[:, :, :, c] += x_pad[i, h * st0: h * st0 + hk,
+                                            w * st1: w * st1 + wk,
+                                            :] * dZ[i, h, w, c]
+    if padding == "same":
+        dX = dX[:, pad0:-pad0, pad1:-pad1, :]
+    return dX, dW, db
