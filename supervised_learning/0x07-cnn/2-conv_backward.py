57840fa645984f60a1989dfdd3df5adbe7e4e28a
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-06-09 12:04:42 +0300

    Add Task 2

--- supervised_learning/0x07-cnn/2-conv_backward.py
+++ supervised_learning/0x07-cnn/2-conv_backward.py
@@ -27,55 +27,40 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
         The partial derivatives with respect to the previous layer (dA_prev),
         the kernels (dW), and the biases (db), respectively
     """
-    m, h_prev, w_prev, c_prev = A_prev.shape
-    kh, kw, c_prev, c_new = W.shape
-    sh, sw = stride
     m, h_new, w_new, c_new = dZ.shape
+    _, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw, _, _ = W.shape
+    sh, sw = stride
 
-    dA_prev = np.zeros(A_prev.shape)
-    dW = np.zeros(W.shape)
-    db = np.sum(dZ, axis=(0, 1, 2))
+    pw, ph = 0, 0
 
-    ph = 0
-    pw = 0
+    db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
 
     if padding == 'same':
-        ph = int(np.ceil(((sh * h_prev) - sh + kh - h_prev) / 2))
-        pw = int(np.ceil(((sw * w_prev) - sw + kw - w_prev) / 2))
+        ph = int(np.ceil(((h_prev-1)*sh+kh-h_prev)/2))
+        pw = int(np.ceil(((w_prev-1)*sw+kw-w_prev)/2))
 
-    A_prev_pad = np.pad(A_prev,
-                        ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-                        'constant')
+    A_prev = np.pad(A_prev,
+                    pad_width=((0, 0),
+                               (ph, ph),
+                               (pw, pw),
+                               (0, 0)),
+                    mode='constant', constant_values=0)
 
-    dA_prev_pad = np.pad(dA_prev,
-                         ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-                         'constant')
+    dA = np.zeros(A_prev.shape)
+    dW = np.zeros(W.shape)
 
-    for i in range(m):
-        a_prev_pad = A_prev_pad[i]
-        da_prev_pad = dA_prev_pad[i]
+    for z in range(m):
+        for y in range(h_new):
+            for x in range(w_new):
+                for k in range(c_new):
+                    aux_W = W[:, :, :, k]
+                    aux_dz = dZ[z, y, x, k]
+                    dA[z, y*sh: y*sh+kh, x*sw: x*sw+kw, :] += aux_dz * aux_W
+                    aux_A_prev = A_prev[z, y*sh: y*sh+kh, x*sw: x*sw+kw, :]
+                    dW[:, :, :, k] += aux_A_prev * aux_dz
 
-        for h in range(h_new):
-            for w in range(w_new):
-                for c in range(c_new):
-                    vert_start = h * sh
-                    vert_end = h * sh + kh
-                    horiz_start = w * sw
-                    horiz_end = w * sw + kw
+    dA = dA[:, ph:dA.shape[1] - ph, pw:dA.shape[2] - pw, :]
 
-                    a_slice = a_prev_pad[vert_start:vert_end,
-                                         horiz_start:horiz_end,
-                                         :]
+    return dA, dW, db
 
-                    da_prev_pad[vert_start:vert_end,
-                                horiz_start:horiz_end,
-                                :] += W[:, :, :, c] * dZ[i, h, w, c]
-
-                    dW[:, :, :, c] += a_slice * dZ[i, h, w, c]
-
-        if padding == "same":
-            dA_prev[i, :, :, :] = da_prev_pad[ph:-ph, pw:-pw, :]
-        else:
-            dA_prev[i, :, :, :] = da_prev_pad[:, :, :]
-
-    return dA_prev, dW, db
