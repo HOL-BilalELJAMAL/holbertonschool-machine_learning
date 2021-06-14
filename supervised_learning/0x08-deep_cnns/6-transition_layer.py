76d11148249410e6cb8b0c56d8188d0d6961c4ec
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-06-14 16:06:38 +0300

    Add Task 6

--- supervised_learning/0x08-deep_cnns/6-transition_layer.py
+++ supervised_learning/0x08-deep_cnns/6-transition_layer.py
@@ -1,1 +1,39 @@
 #!/usr/bin/env python3
+"""
+6-transition_layer.py
+Module that defines a function called transition_layer
+"""
+
+import tensorflow.keras as K
+
+
+def transition_layer(X, nb_filters, compression):
+    """
+    Function that builds a transition layer
+
+    Args:
+        X: output from the previous layer
+        nb_filters: integer representing the number of filters in X
+        compression: compression factor for the transition layer
+
+    Returns:
+        Output of the transition layer and the number of filters within
+        the output
+    """
+    initializer = K.initializers.he_normal(seed=None)
+
+    my_layer = K.layers.BatchNormalization()(X)
+    my_layer = K.layers.Activation('relu')(my_layer)
+
+    nb_filters = int(nb_filters * compression)
+
+    my_layer = K.layers.Conv2D(filters=nb_filters,
+                               kernel_size=1,
+                               padding='same',
+                               kernel_initializer=initializer,
+                               )(my_layer)
+
+    X = K.layers.AveragePooling2D(pool_size=2,
+                                  padding='same')(my_layer)
+
+    return X, nb_filters
