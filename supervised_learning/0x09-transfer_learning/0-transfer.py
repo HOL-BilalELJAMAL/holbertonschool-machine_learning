238dc79b012fc3dff3c0c89ea622014b5a3d6b46
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-06-24 13:24:50 +0300

    Add Task 0

--- supervised_learning/0x09-transfer_learning/0-transfer.py
+++ supervised_learning/0x09-transfer_learning/0-transfer.py
@@ -1,107 +1,65 @@
 #!/usr/bin/env python3
-"""contains:
-    Dataset loading and preprocessing
-    Model creation
-    Model compilation
-    Model training
-    preprocess_data function"""
+"""
+0-transfer.py
+Module that use transfer learning with DenseNet-169 to predict CIFAR-10 dataset
+"""
 
 import tensorflow.keras as K
-import numpy as np
-
-# script should not run when the file is imported
-if __name__ == '__main__':
-    # DATASET LOADING AND PREPROCESSING
-    # dataset loading
-    (x_train, y_train), (x_test, y_test) = K.datasets.cifar10.load_data()
-
-    # pre-processing
-    x_train = K.applications.densenet.preprocess_input(x_train)
-    y_train = K.utils.to_categorical(y_train, 10)
-
-    train_datagen = K.preprocessing.image.ImageDataGenerator(
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True)
-
-    train_gen = train_datagen.flow(x_train, y_train, batch_size=30)
-
-    x_test = K.applications.densenet.preprocess_input(x_test)
-    y_test = K.utils.to_categorical(y_test, 10)
-
-    # MODEL CREATION
-    # input placeholder
-    inputs = K.Input(shape=(32, 32, 3))
-    inputs = K.layers.UpSampling2D()(inputs)
-
-    # use one of the applications listed in Keras Applications
-    network = K.applications.densenet.DenseNet121(include_top=False,
-                                                  pooling='max',
-                                                  input_tensor=inputs,
-                                                  weights='imagenet')
-
-    output = network.layers[-1].output
-    output = K.layers.Flatten()(output)
-    output = K.layers.Dense(512, activation='relu')(output)
-    output = K.layers.Dropout(0.15)(output)
-    output = K.layers.Dense(256, activation='relu')(output)
-    output = K.layers.Dropout(0.15)(output)
-    output = K.layers.Dense(10, activation='softmax')(output)
-
-    model = K.models.Model(network.input, output)
-
-    # MODEL COMPILATION
-    model.compile(optimizer='adam',
-                  loss='categorical_crossentropy',
-                  metrics=['acc'])
-
-    # MODEL TRAINING
-    def learning_rate(epoch):
-        """ updates the learning rate using inverse time decay """
-        return 0.001 / (1 + 1 * epoch)
-
-    # callbacks configuration
-    callbacks = []
-
-    # learning rate
-    lrr__callback = K.callbacks.LearningRateScheduler(learning_rate,
-                                                      verbose=1)
-    callbacks.append(lrr__callback)
-
-    # early stopping
-    es__callback = K.callbacks.EarlyStopping(monitor='val_acc',
-                                             mode='max',
-                                             verbose=1,
-                                             patience=5)
-    callbacks.append(es__callback)
-
-    # checkpoint
-    mc__callback = K.callbacks.ModelCheckpoint('cifar10.h5',
-                                               monitor='val_acc',
-                                               mode='max',
-                                               verbose=1,
-                                               save_best_only=True)
-    callbacks.append(mc__callback)
-
-    history = model.fit(train_gen,
-                        validation_data=(x_test, y_test),
-                        batch_size=128,
-                        callbacks=callbacks,
-                        epochs=32,
-                        verbose=1)
 
 
 def preprocess_data(X, Y):
     """
-    pre-processes the data for your model
-    :param X: numpy.ndarray of shape (m, 32, 32, 3)
-        containing the CIFAR 10 data,
-        where m is the number of data points
-    :param Y: numpy.ndarray of shape (m,) containing the CIFAR 10 labels for X
-    :return: X_p, Y_p
-        X_p is a numpy.ndarray containing the preprocessed X
-        Y_p is a numpy.ndarray containing the preprocessed Y
+    Function that pre-processes the data for the model
+
+    Args:
+        X (numpy.ndarray) of shape (m, 32, 32, 3) containing
+        the CIFAR 10 data, where m is the number of data points
+        Y (numpy.ndarray) of shape (m,) containing the CIFAR 10 labels for X
+    Returns:
+        x_p (numpy.ndarray) containing the preprocessed X
+        y_p (numpy.ndarray) containing the preprocessed Y
     """
-    X_p = K.applications.densenet.preprocess_input(X)
-    Y_p = K.utils.to_categorical(Y, 10)
-    return X_p, Y_p
+    x_p = K.applications.resnet50.preprocess_input(X)
+    y_p = K.utils.to_categorical(Y)
+    return x_p, y_p
+
+
+if __name__ == "__main__":
+
+    def resize_images(X):
+        """Function that resizes data to be accepted by ResNet50 model"""
+        return K.backend.resize_images(X, 7, 7,
+                                       data_format="channels_last",
+                                       interpolation='bilinear')
+    (X_train, Y_train), (X_valid, Y_valid) = K.datasets.cifar10.load_data()
+    optimizer = K.optimizers.Adam(lr=0.00001)
+    ResNet50_model = K.applications.ResNet50(weights='imagenet',
+                                             include_top=False,
+                                             input_shape=(224, 224, 3))
+    ResNet50_model.trainable = False
+    input = K.Input(shape=(32, 32, 3))
+    lambda_1 = K.layers.Lambda(resize_images)(input)
+    x = ResNet50_model(lambda_1, training=False)
+    x = K.layers.Flatten()(x)
+    x = K.layers.BatchNormalization()(x)
+    x = K.layers.Dense(128, activation='relu')(x)
+    x = K.layers.Dropout(0.5)(x)
+    x = K.layers.BatchNormalization()(x)
+    x = K.layers.Dense(64, activation='relu')(x)
+    x = K.layers.Dropout(0.5)(x)
+    x = K.layers.BatchNormalization()(x)
+    x = K.layers.Dense(10, activation='softmax')(x)
+    model = K.Model(input, x)
+    model.compile(optimizer=optimizer,
+                  loss='categorical_crossentropy', metrics=['accuracy'])
+    epochs = 20
+    batch_size = 256
+    xt_prep, yt_prep = preprocess_data(X_train, Y_train)
+    xv_prep, yv_prep = preprocess_data(X_valid, Y_valid)
+    my_callbacks = [K.callbacks.ModelCheckpoint(
+                    filepath='cifar10.h5', save_best_only=True)]
+    model.fit(xt_prep, yt_prep,
+              batch_size=batch_size,
+              validation_data=(xv_prep, yv_prep),
+              epochs=epochs,
+              callbacks=my_callbacks)
