022cd56b23e460fd2e858a8a252ab7f40d052bf5
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-07-04 14:05:40 +0300

    Add Task 1

--- supervised_learning/0x0C-neural_style_transfer/1-neural_style.py
+++ supervised_learning/0x0C-neural_style_transfer/1-neural_style.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-""" NTS """
+"""
+1-neural_style.py
+Module that defines a class NST that performs tasks for neural style transfer
+"""
 import numpy as np
 import tensorflow as tf
 
