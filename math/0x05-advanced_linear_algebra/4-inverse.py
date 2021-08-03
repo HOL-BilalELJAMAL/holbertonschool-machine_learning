4e8175ccaca282733aea7029e68cb437e617f38f
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-08-03 12:53:39 +0300

    Add Task 4

--- math/0x05-advanced_linear_algebra/4-inverse.py
+++ math/0x05-advanced_linear_algebra/4-inverse.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python3
+
 """
 4-inverse.py
-Module that defines a function called inverse
+calculates the inverse of a matrix
 """
 
 
 def inverse(matrix):
-    """
-    Function that calculates the inverse of matrix.
-
-    Args:
-        matrix (list): matrix.
-
-    Returns:
-        The inverse matrix.
-    """
+    """Returns: the inverse of matrix, or None if matrix is singular"""
     deter_mat = determinant(matrix)
     adj_mat = adjugate(matrix)
     if deter_mat is 0:
@@ -27,15 +20,7 @@ def inverse(matrix):
 
 
 def adjugate(matrix):
-    """
-    Function that calculates the adjugate of a square matrix.
-
-    Args:
-        matrix (list): matrix to calculate.
-
-    Returns:
-        The adjugate.
-    """
+    """ FUnction Adjugate"""
     if not isinstance(matrix, list) or len(matrix) == 0:
         raise TypeError("matrix must be a list of lists")
     if not all([isinstance(row, list) for row in matrix]):
@@ -48,13 +33,7 @@ def adjugate(matrix):
 
 def cofactor(matrix):
     """
-    Function that calculates the cofactor of a square matrix.
-
-    Args:
-        matrix (list): matrix to calculate.
-
-    Returns:
-        the cofactor.
+    Returns: the cofactor matrix of matrix
     """
     if not isinstance(matrix, list) or len(matrix) == 0:
         raise TypeError("matrix must be a list of lists")
@@ -71,13 +50,7 @@ def cofactor(matrix):
 
 def minor(matrix):
     """
-    Function that calculates the minor of a square matrix.
-
-    Args:
-        matrix (list): matrix to calculate.
-
-    Returns:
-        The minor.
+    Returns: the minor of matrix
     """
     if not isinstance(matrix, list) or len(matrix) == 0:
         raise TypeError("matrix must be a list of lists")
@@ -93,47 +66,35 @@ def minor(matrix):
     for i in range(len(matrix)):
         minor_row = []
         for j in range(len(matrix[i])):
-            minor_row.append(determinant(getMinor(matrix, i, j)))
+            minor_row.append(determinant(getMatrixMinor(matrix, i, j)))
         result.append(minor_row)
     return result
 
 
-def getMinor(m, i, j):
-    """Function that calculates the minor of a squared matrix"""
+def getMatrixMinor(m, i, j):
+    """calculates the minor of a squared matrix"""
     return [row[:j] + row[j+1:] for row in (m[:i] + m[i+1:])]
 
 
 def determinant(matrix):
     """
-    Function that calculates the determinant of a square matrix.
-
-    Args:
-        matrix (list): matrix to calculate.
-
-    Returns:
-        The determinant.
+    Returns: the determinant of matrix
     """
     if not isinstance(matrix, list) or len(matrix) == 0:
         raise TypeError("matrix must be a list of lists")
-
     if not all([isinstance(row, list) for row in matrix]):
         raise TypeError("matrix must be a list of lists")
-
     if len(matrix) == 1 and len(matrix[0]) == 0:
         return 1
-
     if len(matrix) == 1 and len(matrix[0]) == 1:
         return matrix[0][0]
-
     square = [len(row) == len(matrix) for row in matrix]
     if not all(square):
         raise ValueError('matrix must be a square matrix')
-
     if len(matrix) is 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-
     deter = 0
     for c in range(len(matrix)):
         deter += ((-1) ** c) * matrix[0][c] *\
-                 determinant(getMinor(matrix, 0, c))
+                 determinant(getMatrixMinor(matrix, 0, c))
     return deter
