b1dd62322f996f8d4ade91ba33f9818896dab83c
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-08-03 12:55:37 +0300

    Add Task 4

--- math/0x05-advanced_linear_algebra/4-inverse.py
+++ math/0x05-advanced_linear_algebra/4-inverse.py
@@ -1,106 +1,113 @@
 #!/usr/bin/env python3
-"""module"""
+"""
+Determinant
+"""
+
+
+def minor_m(m, row, col):
+    """Omite the the given row and column of a square matrix.
+    Args:
+        m (list): matrix.
+        row (int): row to omite.
+        col (int): column to omite.
+    Returns:
+        the matrix with the omited row, column.
+    """
+    return [[m[i][j] for j in range(len(m[i])) if j != col]
+            for i in range(len(m)) if i != row]
 
 
 def determinant(matrix):
-    """function"""
-    total = 0
-
-    if not type(matrix) is list or not any(type(i) is list for i in matrix):
+    """ Calculates the determinant of a square matrix.
+    Args:
+        matrix (list): matrix to calculate.
+    Returns:
+        the determinant.
+    """
+    if type(matrix) is not list or len(matrix) == 0:
         raise TypeError("matrix must be a list of lists")
-    if not any(len(i) == len(matrix) for i in matrix) and len(matrix[0]) != 0:
+    if all([type(i) is list for i in matrix]) is False:
+        raise TypeError("matrix must be a list of lists")
+
+    if matrix[0] and len(matrix) != len(matrix[0]):
         raise ValueError("matrix must be a square matrix")
 
-    if len(matrix[0]) == 0:
+    if matrix == [[]]:
         return 1
 
-    if len(matrix[0]) == 1:
+    if len(matrix) == 1:
         return matrix[0][0]
 
-    if len(matrix) == 2 and len(matrix[0]) == 2:
-        det = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-        return det
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
 
-    for col in range(len(matrix[0])):
-        M_rec = matrix.copy()
-        M_rec = M_rec[1:]
-        for row in range(len(M_rec)):
-            M_rec[row] = M_rec[row][:col] + M_rec[row][col+1:]
-        sub_det = determinant(M_rec)
-        sign = (-1)**col
-        total += sign * matrix[0][col] * sub_det
-    return total
+    det = 0
+    for j in range(len(matrix[0])):
+        omited_matrix = minor_m(matrix, 0, j)
+        det += matrix[0][j] * ((-1) ** j) * determinant(omited_matrix)
+
+    return det
 
 
-def minor(matrix):
-    """function"""
-
-    if not type(matrix) is list or not any(type(i) is list for i in matrix):
-        raise TypeError("matrix must be a list of lists")
-    if not any(len(i) == len(matrix) for i in matrix) or len(matrix) == 0:
-        raise ValueError("matrix must be a non-empty square matrix")
-
-    new = [[0 for i in range(len(matrix[0]))] for j in range(len(matrix))]
-    if len(matrix[0]) == 1:
-        new[0][0] = 1
-        return new
-
-    for row in range(len(matrix)):
-        for col in range(len(matrix[0])):
-            M_rec = matrix.copy()
-            M_rec = M_rec[:row] + M_rec[row+1:]
-            for i in range(len(M_rec)):
-                M_rec[i] = M_rec[i][:col] + M_rec[i][col+1:]
-            if len(M_rec) == 1:
-                new[row][col] = M_rec[0][0]
-            else:
-                det = determinant(M_rec)
-                new[row][col] = det
-    return new
-
-
-def cofactor(matrix):
-    """function"""
-    if not type(matrix) is list or not any(type(i) is list for i in matrix):
-        raise TypeError("matrix must be a list of lists")
-    if not any(len(i) == len(matrix) for i in matrix) or len(matrix) == 0:
-        raise ValueError("matrix must be a non-empty square matrix")
-    cofac_matrix = [[0 for i in range(len(matrix[0]))]
-                    for j in range(len(matrix))]
-    minor_matrix = minor(matrix)
-    for i in range(len(minor_matrix)):
-        for j in range(len(minor_matrix[0])):
-            sign = (-1)**(i+j)
-            cofac_matrix[i][j] = sign * minor_matrix[i][j]
-    return cofac_matrix
+def transponse(m):
+    """ Transpose a matrix
+    Args:
+        m (list): matrix.
+    Returns:
+        the transposed matrix
+    """
+    return [[m[row][col] for row in range(len(m))]
+            for col in range(len(m[0]))]
 
 
 def adjugate(matrix):
-    """function"""
-    if not type(matrix) is list or not any(type(i) is list for i in matrix):
+    """ Calculates the adjugate of a square matrix.
+    Args:
+        matrix (list): matrix to calculate.
+    Returns:
+        the adjugate.
+    """
+    if type(matrix) is not list or len(matrix) == 0:
         raise TypeError("matrix must be a list of lists")
-    if not any(len(i) == len(matrix) for i in matrix) or len(matrix) == 0:
+    if all([type(i) is list for i in matrix]) is False:
+        raise TypeError("matrix must be a list of lists")
+
+    if (matrix[0] and len(matrix) != len(matrix[0])) or \
+            matrix == [] or matrix == [[]]:
         raise ValueError("matrix must be a non-empty square matrix")
-    adj = [[0 for i in range(len(matrix[0]))] for j in range(len(matrix))]
-    cofac = cofactor(matrix)
-    for i in range(len(cofac)):
-        for j in range(len(cofac[0])):
-            adj[i][j] = cofac[j][i]
-    return adj
+
+    if len(matrix) == 1 and len(matrix[0]) == 1:
+        return [[1]]
+
+    cofactor = [[((-1) ** (i + j)) * determinant(minor_m(matrix, i, j))
+                 for j in range(len(matrix[i]))] for i in range(len(matrix))]
+
+    return transponse(cofactor)
 
 
 def inverse(matrix):
-    """function"""
-    if not type(matrix) is list or not all(type(i) is list for i in matrix):
+    """Calculate inverse matrix.
+    Args:
+        matrix (list): matrix.
+    Returns:
+        inverse matrix.
+    """
+    if type(matrix) is not list or len(matrix) == 0:
         raise TypeError("matrix must be a list of lists")
-    if not all(len(i) == len(matrix) for i in matrix) or len(matrix) == 0:
+    if all([type(i) is list for i in matrix]) is False:
+        raise TypeError("matrix must be a list of lists")
+
+    if (len(matrix) == 0 or len(matrix) != len(matrix[0])) \
+            or matrix == [[]]:
         raise ValueError("matrix must be a non-empty square matrix")
+
+    if any([len(l) != len(matrix) for l in matrix]):
+        raise ValueError("matrix must be a non-empty square matrix")
+
     det = determinant(matrix)
     if det == 0:
         return None
-    adj = adjugate(matrix)
-    inv = [[0 for i in range(len(matrix[0]))] for j in range(len(matrix))]
-    for i in range(len(inv)):
-        for j in range(len(inv[0])):
-            inv[i][j] = (1/det) * adj[i][j]
-    return inv
+
+    adjugated = adjugate(matrix)
+
+    return [[n / det for n in row] for row in adjugated]
