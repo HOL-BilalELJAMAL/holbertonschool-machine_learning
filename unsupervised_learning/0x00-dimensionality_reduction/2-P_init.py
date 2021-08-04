815956bf3f71a6e30b27b267e079161a652079c0
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-08-04 13:23:02 +0300

    Add Task 2

--- unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
+++ unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
@@ -9,15 +9,31 @@ import numpy as np
 
 def P_init(X, perplexity):
     """
-    Function that initializes all variables required to calculate the
-    P affinities in t-SNE
+    Function that initializes all variables required to calculate
+    the P affinities in t-SNE
+
+    Args:
+        X - is a numpy.ndarray of shape (n, d) containing the dataset to
+        be transformed by t-SNE
+            n - is the number of data points
+            d - is the number of dimensions in each point
+        perplexity - is the perplexity that all Gaussian distributions
+        should have
+
+    Returns: (D, P, betas, H)
+        D: a numpy.ndarray of shape (n, n) that calculates the pairwise
+            distance between two data points
+        P: a numpy.ndarray of shape (n, n) initialized to all 0‘s that will
+            contain the P affinities
+        betas: a numpy.ndarray of shape (n, 1) initialized to all 1’s that will
+            contain all of the beta values
+            beta_i = 1/(2*sigma_i^2)
+        H is the Shannon entropy for perplexity perplexity
     """
-    n = X.shape[0]
-    X_sum = np.sum(np.square(X), 1)
-    D = np.add(np.add(-2 * np.dot(X, X.T), X_sum).T, X_sum)
-    np.fill_diagonal(D, 0.)
-    betas = np.ones((n, 1))
-    P = np.zeros((n, n))
-    P_sum = sum(P)
+    n, d = X.shape
+    sum_X = np.sum(np.square(X), 1)
+    D = np.add(np.add(-2 * np.dot(X, X.T), sum_X).T, sum_X)
+    P = np.zeros([n, n], dtype='float64')
+    betas = np.ones([n, 1], dtype='float64')
     H = np.log2(perplexity)
-    return (D, P, betas, H)
+    return D, P, betas, H
