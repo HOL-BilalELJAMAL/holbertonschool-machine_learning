bb81cb1b0d7c761e8ec6bd842f52d1ee0de33261
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-08-04 13:27:41 +0300

    Add Task 5

--- unsupervised_learning/0x00-dimensionality_reduction/5-Q_affinities.py
+++ unsupervised_learning/0x00-dimensionality_reduction/5-Q_affinities.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python3
+"""
+5-Q_affinities.py
+Module that defines a function called Q_affinities
+"""
+
+import numpy as np
+
+
+def Q_affinities(Y):
+    """
+    Function that calculates the Q affinities
+
+    Args:
+        Y is a numpy.ndarray of shape (n, ndim) containing the low
+            dimensional transformation of X
+
+    Returns: Q, num
+        Q - is a numpy.ndarray of shape (n, n) containing the Q affinities
+        num - is a numpy.ndarray of shape (n, n) containing the numerator
+            of the Q affinities
+    """
+    n, ndim = Y.shape
+    sum_Y = np.sum(np.square(Y), axis=1)
+    num = -2. * np.dot(Y, Y.T)
+    num = 1. / (1. + np.add(np.add(num, sum_Y).T, sum_Y))
+    num[range(n), range(n)] = 0.
+    Q = num / np.sum(num)
+    return Q, num
