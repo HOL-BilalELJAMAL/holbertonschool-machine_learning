7b333bd714750043406379748852d79442841f83
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-08-04 13:09:48 +0300

    Add Task 0

--- unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+"""
+0-pca.py
+Module that defines a function called pca
+"""
+
+import numpy as np
+
+
+def pca(X, var=0.95):
+    """
+    Function that performs PCA on a dataset
+    """
+    u, s, vh = np.linalg.svd(X)
+    total_variance = np.cumsum(s) / np.sum(s)
+    r = (np.argwhere(total_variance >= var))[0, 0]
+    weight = vh[:r + 1].T
+    return weight
