303a9c52fa171464ccf6f6432762c98db8854d4a
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-08-06 12:56:18 +0300

    Add Task 8

--- unsupervised_learning/0x00-dimensionality_reduction/8-tsne.py
+++ unsupervised_learning/0x00-dimensionality_reduction/8-tsne.py
@@ -28,23 +28,29 @@ def tsne(X, ndims=2, idims=50, perplexity=30.0, iterations=1000, lr=500):
         Y, a numpy.ndarray of shape (n, ndim) containing the optimized low
         dimensional transformation of X
     """
+    n, d = X.shape
     X = pca(X, idims)
-    n, _ = X.shape
-    P = P_affinities(X, perplexity=perplexity) * 4
+    P = P_affinities(X, perplexity=perplexity)
     Y = np.random.randn(n, ndims)
-    iY = np.zeros((n, ndims))
-    for i in range(1, iterations + 1):
+    iY = Y
+    P = P * 4.
+
+    for i in range(iterations):
         dY, Q = grads(Y, P)
         if i <= 20:
             momentum = 0.5
         else:
             momentum = 0.8
+
         iY = momentum * iY - lr * dY
         Y = Y + iY
         Y = Y - np.tile(np.mean(Y, 0), (n, 1))
-        if i != 0 and i % 100 == 0:
+
+        if (i + 1) % 100 == 0:
             C = cost(P, Q)
-            print("Cost at iteration {}: {}".format(i, C))
+            print('Cost at iteration {}: {}'.format((i+1), C))
+
         if i == 100:
             P = P / 4.
+
     return Y
