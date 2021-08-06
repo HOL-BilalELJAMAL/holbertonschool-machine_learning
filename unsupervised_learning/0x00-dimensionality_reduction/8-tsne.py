df7a972fdaacc2f33ed4f0442cebe3057267fe95
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-08-06 12:42:46 +0300

    Add Task 8

--- unsupervised_learning/0x00-dimensionality_reduction/8-tsne.py
+++ unsupervised_learning/0x00-dimensionality_reduction/8-tsne.py
@@ -28,34 +28,37 @@ def tsne(X, ndims=2, idims=50, perplexity=30.0, iterations=1000, lr=500):
         Y, a numpy.ndarray of shape (n, ndim) containing the optimized low
         dimensional transformation of X
     """
-    n, d = X.shape
     initial_momentum = 0.5
     final_momentum = 0.8
+    min_gain = 0.1
 
-    X = pca(X, idims)
-    P = P_affinities(X, perplexity=perplexity)
-    P = P * 4
-
+    n, d = X.shape
+    gains = np.ones((n, ndims))
     Y = np.random.randn(n, ndims)
+    # Y - np.mean(Y, axis=0)
     iY = np.zeros((n, ndims))
 
-    for i in range(iterations):
+    pca_result = pca(X, idims)
+    P = P_affinities(pca_result, perplexity=perplexity)
+    # Exaggeration
+    P = 4 * P
 
+    for i in range(iterations):
         dY, Q = grads(Y, P)
-        if i < (20):
+        if i < 20:
             momentum = initial_momentum
         else:
             momentum = final_momentum
 
         iY = momentum * iY - lr * dY
-        Y = Y + iY
-        Y = Y - np.tile(np.mean(Y, 0), (n, 1))
+        Y = Y + iY - np.tile(np.mean(Y, 0), (n, 1))
 
         if (i + 1) % 100 == 0:
             C = cost(P, Q)
-            print("Cost at iteration {}: {}".format((i+1), C))
+            print("Cost at iteration {}: {}".format(i + 1, C))
 
+        # Remove exaggeration
         if (i + 1) == 100:
             P = P / 4
 
-    return (Y)
+    return Y
