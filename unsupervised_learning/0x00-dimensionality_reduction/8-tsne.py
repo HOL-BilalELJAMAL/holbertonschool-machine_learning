5c2e7b11d71f66635582e75033ae8c7b438118d1
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-08-06 12:20:15 +0300

    Add Task 8

--- unsupervised_learning/0x00-dimensionality_reduction/8-tsne.py
+++ unsupervised_learning/0x00-dimensionality_reduction/8-tsne.py
@@ -14,7 +14,6 @@ cost = __import__('7-cost').cost
 def tsne(X, ndims=2, idims=50, perplexity=30.0, iterations=1000, lr=500):
     """
     Function that performs a t-SNE transformation
-
     Args:
         X: is a numpy.ndarray of shape (n, d) containing the dataset to be
             transformed by t-SNE
@@ -25,30 +24,36 @@ def tsne(X, ndims=2, idims=50, perplexity=30.0, iterations=1000, lr=500):
         perplexity - is the perplexity
         iterations - is the number of iterations
         lr - is the learning rate
-
     Returns:
         Y, a numpy.ndarray of shape (n, ndim) containing the optimized low
         dimensional transformation of X
     """
-    X = pca(X, idims)
-    n, _ = X.shape
     initial_momentum = 0.5
     final_momentum = 0.8
-    P = P_affinities(X, perplexity=perplexity) * 4
+
+    n, d = X.shape
     Y = np.random.randn(n, ndims)
     iY = np.zeros((n, ndims))
-    for i in range(1, iterations + 1):
+
+    pca_result = pca(X, idims)
+    P = P_affinities(pca_result, perplexity=perplexity)
+    P = 4 * P
+
+    for i in range(iterations):
         dY, Q = grads(Y, P)
         if i < 20:
             momentum = initial_momentum
         else:
             momentum = final_momentum
+
         iY = momentum * iY - lr * dY
-        Y = Y + iY
-        Y = Y - np.tile(np.mean(Y, 0), (n, 1))
-        if i != 0 and i % 100 == 0:
+        Y = Y + iY - np.tile(np.mean(Y, 0), (n, 1))
+
+        if (i + 1) % 100 == 0:
             C = cost(P, Q)
-            print("Cost at iteration {}: {}".format(i, C))
-        if i == 100:
-            P = P / 4.
+            print("Cost at iteration {}: {}".format(i + 1, C))
+
+        if (i + 1) == 100:
+            P = P / 4
+
     return Y
