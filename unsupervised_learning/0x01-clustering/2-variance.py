80d41217afcdc41a6cff17fdd2ae17164c017608
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-08-10 09:43:01 +0300

    Add Task 2

--- unsupervised_learning/0x01-clustering/2-variance.py
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -1,1 +1,31 @@
 #!/usr/bin/env python3
+"""
+2-variance.py
+Module that defines a function called variance
+"""
+
+import numpy as np
+
+
+def variance(X, C):
+    """
+    Function that calculates the total intra-cluster variance
+    for a data set
+
+    Args:
+        X: numpy.ndarray of shape (n, d) containing the data set
+        C: numpy.ndarray of shape (k, d) containing the centroid means
+        for each cluster
+
+    Returns:
+        var, or None on failure
+    """
+    try:
+        a2 = np.sum(C ** 2, axis=1)[:, np.newaxis]
+        b2 = np.sum(X ** 2, axis=1)
+        ab = np.matmul(C, X.T)
+        SED = a2 - 2 * ab + b2
+        var = np.sum(np.amin(SED, axis=0))
+        return var
+    except Exception:
+        return None
