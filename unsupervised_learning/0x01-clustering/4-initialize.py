97854b1e566bf73e718cf4519b8569861f6f5cdb
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-08-10 09:49:08 +0300

    Add Task 4

--- unsupervised_learning/0x01-clustering/4-initialize.py
+++ unsupervised_learning/0x01-clustering/4-initialize.py
@@ -1,1 +1,42 @@
 #!/usr/bin/env python3
+"""
+4-initialize.py
+Module that defines a function called initialize
+"""
+
+import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+
+
+def initialize(X, k):
+    """
+    Function that initializes cluster centroids for K-means
+
+    Args:
+        X: numpy.ndarray of shape (n, d) containing the dataset that will
+            be used for K-means clustering:
+            - n is the number of data points
+            - d is the number of dimensions for each data point
+        k: positive integer containing the number of clusters
+
+    Returns:
+        pi, m, S, or None, None, None on failure
+        pi is a numpy.ndarray of shape (k,) containing the priors for each
+        cluster, initialized evenly
+        m is a numpy.ndarray of shape (k, d) containing the centroid means
+        for each cluster, initialized with K-means
+        S is a numpy.ndarray of shape (k, d, d) containing the covariance
+        matrices for each cluster, initialized as identity matrices
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None, None
+    if type(k) != int or k <= 0 or k >= X.shape[0]:
+        return None, None, None
+
+    n, d = X.shape
+    pi = np.tile(1/k, (k,))
+    m, _ = kmeans(X, k)
+    S_ = np.identity(d)
+    S = np.tile(S_, (k, 1, 1))
+
+    return pi, m, S
