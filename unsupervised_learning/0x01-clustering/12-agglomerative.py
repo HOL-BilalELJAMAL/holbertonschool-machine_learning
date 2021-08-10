687099a96aa58b7faf8e5dc32ac341ae57a66d25
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-08-10 10:21:05 +0300

    Add Task 12

--- unsupervised_learning/0x01-clustering/12-agglomerative.py
+++ unsupervised_learning/0x01-clustering/12-agglomerative.py
@@ -1,1 +1,31 @@
 #!/usr/bin/env python3
+"""
+12-agglomerative.py
+Module that defines a function called agglomerative
+"""
+
+import scipy.cluster.hierarchy
+import matplotlib.pyplot as plt
+
+
+def agglomerative(X, dist):
+    """
+    Function that performs agglomerative clustering on a dataset
+
+    Args:
+        X: numpy.ndarray of shape (n, d) containing the dataset
+        dist: maximum cophenetic distance for all clusters
+
+    Returns:
+        clss, a numpy.ndarray of shape (n,) containing the cluster
+        indices for each data point
+    """
+
+    Z = scipy.cluster.hierarchy.linkage(X, method='ward')
+    fig = plt.figure(figsize=(25, 10))
+    dn = scipy.cluster.hierarchy.dendrogram(Z, color_threshold=dist)
+    plt.show()
+    clss = scipy.cluster.hierarchy.fcluster(Z,
+                                            t=dist,
+                                            criterion='distance')
+    return clss
