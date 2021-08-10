37c963284449e17811de0c3e569880e8b60c8fd2
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-08-10 09:46:07 +0300

    Add Task 3

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -1,1 +1,59 @@
 #!/usr/bin/env python3
+"""
+3-optimum.py
+Module that defines a function called optimum_k
+"""
+
+import numpy as np
+
+kmeans = __import__('1-kmeans').kmeans
+variance = __import__('2-variance').variance
+
+
+def optimum_k(X, kmin=1, kmax=None, iterations=1000):
+    """
+    Function that tests for the optimum number of clusters
+    by variance
+    Args:
+        X: numpy.ndarray of shape (n, d) containing the data set
+        kmin: positive integer containing the minimum number of clusters
+                to check for (inclusive)
+        kmax:  positive integer containing the maximum number of clusters
+                to check for (inclusive)
+        iterations: positive integer containing the maximum number of
+                iterations for K-means
+
+    Returns:
+        results, d_vars, or None, None on failure
+        results is a list containing the outputs of K-means for each
+        cluster size
+        d_vars is a list containing the difference in variance from the
+        smallest cluster size for each cluster size
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None
+    if kmax is None:
+        kmax = X.shape[0]
+    if type(kmin) != int or kmin <= 0 or X.shape[0] <= kmin:
+        return None, None
+    if type(kmax) != int or kmax <= 0 or X.shape[0] < kmax:
+        return None, None
+    if kmax <= kmin:
+        return None, None
+    if type(iterations) != int or iterations <= 0:
+        return None, None
+
+    results = []
+    d_vars = []
+
+    for k in range(kmin, kmax + 1):
+        C, clss = kmeans(X, k, iterations)
+        results.append((C, clss))
+
+        if k == kmin:
+            initial_var = variance(X, C)
+
+        var = variance(X, C)
+        d_vars.append(initial_var - var)
+
+    return results, d_vars
