6adc43198dee1d8a866a5f68e921bd43d1881920
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-08-10 09:51:32 +0300

    Add Task 5

--- unsupervised_learning/0x01-clustering/5-pdf.py
+++ unsupervised_learning/0x01-clustering/5-pdf.py
@@ -1,1 +1,55 @@
 #!/usr/bin/env python3
+"""
+5-pdf.py
+Module that defines a function called pdf
+"""
+
+import numpy as np
+
+
+def pdf(X, m, S):
+    """
+    Function that calculates the probability density function of a
+    Gaussian distribution
+
+    Args:
+        X: numpy.ndarray of shape (n, d) containing the data points whose
+        PDF should be evaluated
+        m: numpy.ndarray of shape (d,) containing the mean of the distribution
+        S: numpy.ndarray of shape (d, d) containing the covariance of the
+        distribution
+
+    Returns:
+        P, or None on failure
+        P is a numpy.ndarray of shape (n,) containing the PDF values
+        for each data point
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    if not isinstance(m, np.ndarray) or len(m.shape) != 1:
+        return None
+    if not isinstance(S, np.ndarray) or len(S.shape) != 2:
+        return None
+    if X.shape[1] != m.shape[0] or X.shape[1] != S.shape[0]:
+        return None
+    if S.shape[0] != S.shape[1]:
+        return None
+
+    n, d = X.shape
+
+    Q = np.linalg.inv(S)
+    det = np.linalg.det(S)
+
+    den = np.sqrt(((2 * np.pi) ** d) * det)
+
+    diff = X.T - m[:, np.newaxis]
+
+    M1 = np.matmul(Q, diff)
+    M2 = np.sum(diff * M1, axis=0)
+    M3 = - M2 / 2
+
+    density = np.exp(M3) / den
+
+    density = np.where(density < 1e-300, 1e-300, density)
+
+    return density
