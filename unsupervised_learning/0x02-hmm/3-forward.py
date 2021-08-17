8a97bf878c505435cc62ad8c6ce8b101b32be1d3
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-08-17 23:10:07 +0300

    Add Task 3

--- unsupervised_learning/0x02-hmm/3-forward.py
+++ unsupervised_learning/0x02-hmm/3-forward.py
@@ -1,1 +1,83 @@
 #!/usr/bin/env python3
+"""
+3-forward.py
+Module that defines a function called forward
+"""
+import numpy as np
+
+
+def forward(Observation, Emission, Transition, Initial):
+    """
+    Function that performs the forward algorithm for a hidden
+    markov model
+
+    Args:
+        Observation: numpy.ndarray of shape (T,)
+        that contains the index of the observation
+        T is the number of observations
+        Emission: numpy.ndarray of shape (N, M)
+        containing the emission probability of a specific observation
+        given a hidden state
+        Emission[i, j] is the probability of observing j given the
+        hidden state i
+        N is the number of hidden states
+        M is the number of all possible observations
+        Transition: 2D numpy.ndarray of shape (N, N)
+        containing the transition probabilities
+        Transition[i, j] is the probability of transitioning from the
+        hidden state i to j
+        Initial: numpy.ndarray of shape (N, 1) containing the probability
+        of starting in a particular hidden state
+
+    Returns:
+        P, F, or None, None on failure
+        P is the likelihood of the observations given the model
+        F is a numpy.ndarray of shape (N, T)
+        containing the forward path probabilities
+        F[i, j] is the probability of being in hidden state i at time j
+        given the previous observations
+    """
+    if not isinstance(Observation, np.ndarray) or len(Observation.shape) != 1:
+        return None, None
+
+    if not isinstance(Emission, np.ndarray) or len(Emission.shape) != 2:
+        return None, None
+
+    if not isinstance(Transition, np.ndarray) or len(Transition.shape) != 2:
+        return None, None
+
+    if not isinstance(Initial, np.ndarray) or len(Initial.shape) != 2:
+        return None, None
+
+    T = Observation.shape[0]
+
+    N, M = Emission.shape
+
+    if Transition.shape[0] != N or Transition.shape[1] != N:
+        return None, None
+
+    if Initial.shape[0] != N or Initial.shape[1] != 1:
+        return None, None
+
+    if not np.sum(Emission, axis=1).all():
+        return None, None
+    if not np.sum(Transition, axis=1).all():
+        return None, None
+    if not np.sum(Initial) == 1:
+        return None, None
+
+    F = np.zeros((N, T))
+
+    Obs_i = Observation[0]
+    prob = np.multiply(Initial[:, 0], Emission[:, Obs_i])
+    F[:, 0] = prob
+
+    for i in range(1, T):
+        Obs_i = Observation[i]
+        state = np.matmul(F[:, i - 1], Transition)
+        prob = np.multiply(state, Emission[:, Obs_i])
+        F[:, i] = prob
+
+    P = np.sum(F[:, T - 1])
+
+    return P, F
