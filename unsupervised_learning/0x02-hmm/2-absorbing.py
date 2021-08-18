7d4bf768c197cadc1d23a5416a6df383865a343c
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-08-18 12:14:57 +0300

    Add Task 2

--- unsupervised_learning/0x02-hmm/2-absorbing.py
+++ unsupervised_learning/0x02-hmm/2-absorbing.py
@@ -6,51 +6,40 @@ Module that defines a function called absorbing
 import numpy as np
 
 
-def route_check(P, n):
-    """
-    Function that checks if it is possible to go from each non-absorbing
-    state to at least one absorbing state
-    """
-    absorbing_states = np.where(np.diag(P) == 1)
-    rows = P[absorbing_states[0]]
-    account = np.sum(rows, axis=0)
-    for i in range(n):
-        row_check = P[i] != 0
-        intersection = account * row_check
-        if (intersection == 1).any():
-            account[i] = 1
-    return account.all()
-
-
 def absorbing(P):
     """
     Function that determines if a markov chain is absorbing
-
     Args:
         P: P[i, j] is the probability of transitioning
         from state i to state j
         n is the number of states in the markov chain
-
     Returns:
         True if it is absorbing, or False on failure
     """
-    if not isinstance(P, np.ndarray) or len(P.shape) != 2:
+    if type(P) is not np.ndarray or len(P.shape) != 2:
         return False
-
     if P.shape[0] != P.shape[1]:
         return False
 
-    if np.sum(P, axis=1).all() != 1:
-        return False
+    sum_test = np.sum(P, axis=1)
+    for elem in sum_test:
+        if not np.isclose(elem, 1):
+            return None
 
-    n = P.shape[0]
-
-    P_diag = np.diag(P)
-
-    if all(P_diag == 1):
+    n, _ = P.shape
+    D = np.diag(P)
+    if (D == 1).all():
         return True
-
-    if not any(P_diag == 1):
+    if (D != 1).all():
         return False
 
-    return route_check(P, n)
+    absorb = np.where(D == 1, 1, 0)
+    for i in range(n):
+        idx = np.where(absorb == 1)
+        for ind in idx[0]:
+            elem = P[:, ind]
+            mask = np.where(elem > 0)[0]
+            absorb[mask] = True
+            if absorb.all():
+                return True
+    return absorb.all()
