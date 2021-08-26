4105984aa31ee53ffc38217def5d6c20d9cfd91a
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-08-26 14:41:32 +0300

    Add Task 5

--- unsupervised_learning/0x03-hyperparameter_tuning/5-bayes_opt.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/5-bayes_opt.py
@@ -9,83 +9,73 @@ from scipy.stats import norm
 GP = __import__('2-gp').GaussianProcess
 
 
-class BayesianOptimization:
-    """BayesianOptimization Class"""
+class BayesianOptimization(object):
+    """ BayesianOptimization class """
 
     def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1,
                  sigma_f=1, xsi=0.01, minimize=True):
-        """
-        Class constructor
-
-        Args:
-            f: black-box function to be optimized
-            X_init: numpy.ndarray of shape (t, 1)
-            representing the inputs already sampled with the black-box function
-            Y_init: numpy.ndarray of shape (t, 1) representing the outputs
-            of the black-box function for each input in X_init
-            bounds: tuple of (min, max) representing the bounds
-            of the space in which to look for the optimal point
-            ac_samples: number of samples that should be analyzed
-            during acquisition
-            l: length parameter for the kernel
-            sigma_f: standard deviation given to the output of the
-            black-box function
-            xsi: exploration-exploitation factor for acquisition
-            minimize: bool determining whether optimization
-            should be performed for minimization (True) or maximization (False)
+        """ Initializer.
+            Args:
+                f: (numpy.ndarray) the black-box function to be optimized.
+                X_init: (numpy.ndarray) representing the inputs already
+                        sampled with the black-box function.
+                Y_init: (numpy.ndarray) representing the outputs of the
+                        black-box function for each input in X_init.
+                bounds: (tuple) containing (min, max) representing the bounds
+                        of the space in which to look for the optimal point.
+                ac_sample: (int) the number of samples that should be
+                           analyzed during acquisition.
+                l: (int) the length parameter for the kernel.
+                sigma_f: (float) the standard deviation given to the output
+                         of the black-box function.
+                xsi: (float) the exploration-exploitation factor
+                     for acquisition.
+                minimize: (bool) determining whether optimization should be
+                          performed for minimization (True)
+                          or maximization (False).
         """
         self.f = f
         self.gp = GP(X_init, Y_init, l, sigma_f)
-        _min, _max = bounds
-        self.X_s = np.linspace(_min, _max, ac_samples).reshape(-1, 1)
+        self.X_s = np.linspace(bounds[0], bounds[1], ac_samples)[:, np.newaxis]
         self.xsi = xsi
         self.minimize = minimize
 
     def acquisition(self):
+        """ calculates the next best sample location.
+            Args:
+                None.
+            Returns:
+                X_next: (numpy.ndarray) representing the next best
+                        sample point.
+                EI: (numpy.ndarray) containing the expected improvement
+                                    of each potential sample.
         """
-        Function that calculates the next best sample location
-
-        Returns:
-            X_next, EI
-            X_next is a numpy.ndarray of shape (1,)
-                representing the next best sample point
-            EI is a numpy.ndarray of shape (ac_samples,)
-                containing the expected improvement of each potential sample
-        """
-        X = self.gp.X
-        mu_sample, _ = self.gp.predict(X)
-
         mu, sigma = self.gp.predict(self.X_s)
-
-        sigma = sigma.reshape(-1, 1)
+        mu_sample, _ = self.gp.predict(self.gp.X)
 
         with np.errstate(divide='warn'):
-            if self.minimize is True:
-                mu_sample_opt = np.amin(self.gp.Y)
-                imp = (mu_sample_opt - mu - self.xsi).reshape(-1, 1)
+            if self.minimize:
+                mu_sample_opt = np.min(mu_sample)
+                imp = mu_sample_opt - mu - self.xsi
             else:
-                mu_sample_opt = np.amax(self.gp.Y)
-                imp = (mu - mu_sample_opt - self.xsi).reshape(-1, 1)
-
+                mu_sample_opt = np.max(mu_sample)
+                imp = mu - mu_sample_opt - self.xsi
             Z = imp / sigma
             EI = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
             EI[sigma == 0.0] = 0.0
 
         X_next = self.X_s[np.argmax(EI)]
 
-        return X_next, EI.reshape(-1)
+        return X_next, EI
 
     def optimize(self, iterations=100):
-        """
-        Function that optimizes the black-box function
-
-        Args:
-            iterations:  maximum number of iterations to perform
-            X_opt, Y_opt
-            X_opt is a numpy.ndarray of shape (1,)
-                representing the optimal point
-            Y_opt is a numpy.ndarray of shape (1,)
-                representing the optimal function value
+        """ optimizes the black-box function.
+            Args:
+                iterations: (int) the maximum number of
+                            iterations to perform.
+            Returns:
+                X_opt: (numpy.ndarray) representing the optimal point.
+                Y_opt: (numpy.ndarray) representing the optimal function value.
         """
         for i in range(iterations):
             X_next, _ = self.acquisition()
