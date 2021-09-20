9cbba0d78702329f35a5e6f22c6477c6460f71b6
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-09-20 11:14:51 +0300

    Add Task 0

--- supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
+++ supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""
+0-bag_of_words.py
+Module that defines a function called bag_of_words
+"""
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+def bag_of_words(sentences, vocab=None):
+    """Function that produces an array of embeddings from a corpus"""
+    vectorizer = CountVectorizer(vocabulary=vocab)
+    X = vectorizer.fit_transform(sentences)
+    embeddings = X.toarray()
+    features = vectorizer.get_feature_names()
+    return embeddings, features
