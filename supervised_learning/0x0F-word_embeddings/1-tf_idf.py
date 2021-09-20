6f59d8baff6c225ea88e84613e488038adc36939
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-09-20 11:15:55 +0300

    Add Task 1

--- supervised_learning/0x0F-word_embeddings/1-tf_idf.py
+++ supervised_learning/0x0F-word_embeddings/1-tf_idf.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""
+1-tf_idf.py
+Module that defines a function called tf_idf
+"""
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+def tf_idf(sentences, vocab=None):
+    """Function that produces an array of embeddings from a corpus"""
+    vectorizer = TfidfVectorizer(vocabulary=vocab)
+    X = vectorizer.fit_transform(sentences)
+    embeddings = X.toarray()
+    features = vectorizer.get_feature_names()
+    return embeddings, features
