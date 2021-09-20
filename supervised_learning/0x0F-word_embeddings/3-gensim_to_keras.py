c88327c150b6a7767bbda5417dc34a6843e3f5f1
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-09-20 11:20:17 +0300

    Add Task 3

--- supervised_learning/0x0F-word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/0x0F-word_embeddings/3-gensim_to_keras.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""
+3-gensim_to_keras.py
+"""
+
+from gensim.models import Word2Vec
+
+
+def gensim_to_keras(model):
+    """
+    Function that converts a gensim word2vec model to a trainable keras layer
+    """
+    return model.wv.get_keras_embedding(train_embeddings=True)
