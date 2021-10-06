9990af282c97167df92df55c4fc951eccc07fb1d
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-10-06 12:59:44 +0300

    Add Task 4

--- supervised_learning/0x13-qa_bot/4-qa.py
+++ supervised_learning/0x13-qa_bot/4-qa.py
@@ -1,1 +1,30 @@
 #!/usr/bin/env python3
+"""
+4-qa.py
+Module that defines a function called qa_bot
+"""
+
+question_answer = __import__('0-qa').question_answer
+semantic_search = __import__('3-semantic_search').semantic_search
+
+
+def qa_bot(corpus_path):
+    """
+    Function that answers questions from multiple reference texts
+
+    Args:
+        corpus_path is the path to the corpus of reference documents
+    """
+    while True:
+        question = input('Q: ')
+        question = question.lower()
+        if question == 'exit' or question == 'quit' or\
+                question == 'goodbye' or question == 'bye':
+            print('A: Goodbye')
+            break
+        else:
+            reference = semantic_search(corpus_path, question)
+            answer = question_answer(question, reference)
+            if answer is None:
+                answer = 'Sorry, I do not understand your question.'
+            print('A: {}'.format(answer))
