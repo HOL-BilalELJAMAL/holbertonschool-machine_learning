093935b8b29dbef27dd8fecc24fd5c48d8c6d038
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-10-28 10:15:07 +0300

    Add Task 0

--- reinforcement_learning/0x00-q_learning/0-load_env.py
+++ reinforcement_learning/0x00-q_learning/0-load_env.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+"""
+0-load_env.py
+Module that defines a function called load_frozen_lake
+"""
+
+import numpy as np
+import gym
+
+
+def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
+    """
+    Function that loads a pre-made FrozenLakeEnv environment from OpenAI’s gym
+    """
+
+    env = gym.make("FrozenLake-v0",
+                   desc=desc,
+                   map_name=map_name,
+                   is_slippery=is_slippery)
+
+    return env
