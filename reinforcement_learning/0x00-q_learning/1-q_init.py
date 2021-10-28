8ecfd145dd4f458f2f7816754a6120516dc10ac7
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-10-28 10:15:55 +0300

    Add Task 1

--- reinforcement_learning/0x00-q_learning/1-q_init.py
+++ reinforcement_learning/0x00-q_learning/1-q_init.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+"""
+1-q_init.py
+Module that defines a function called q_init
+"""
+
+import numpy as np
+import gym
+
+
+def q_init(env):
+    """
+    Function that initializes the Q-table
+    """
+    action_space_size = env.action_space.n
+    state_space_size = env.observation_space.n
+    q_table = np.zeros(shape=(state_space_size, action_space_size))
+    return q_table
