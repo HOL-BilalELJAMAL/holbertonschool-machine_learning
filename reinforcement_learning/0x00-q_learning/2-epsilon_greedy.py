2b9aecceaf06f81a37639ad476879e64358b4eab
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-10-28 10:17:19 +0300

    Add Task 2

--- reinforcement_learning/0x00-q_learning/2-epsilon_greedy.py
+++ reinforcement_learning/0x00-q_learning/2-epsilon_greedy.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+"""
+2-epsilon_greedy.py
+Module that defines a function called epsilon_greedy
+"""
+
+import numpy as np
+import gym
+
+
+def epsilon_greedy(Q, state, epsilon):
+    """
+    Function that uses epsilon-greedy to determine the next action
+    """
+    exploration_rate_threshold = np.random.uniform(0, 1)
+    if exploration_rate_threshold > epsilon:
+        action = np.argmax(Q[state, :])
+    else:
+        action = np.random.randint(Q.shape[1])
+    return action
