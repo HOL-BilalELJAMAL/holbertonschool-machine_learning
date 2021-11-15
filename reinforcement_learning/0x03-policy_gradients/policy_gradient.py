685b0de3ec89bf5b5c4f2d36f9761a96d1bafb85
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-11-15 10:11:35 +0200

    Add policy_gradient.py

--- reinforcement_learning/0x03-policy_gradients/policy_gradient.py
+++ reinforcement_learning/0x03-policy_gradients/policy_gradient.py
@@ -1,1 +1,48 @@
-# 0x03-policy_gradients
+#!/usr/bin/env python3
+"""
+policy_gradient.py
+Function that defines function to compute Monte Carlo policy gradient
+based on state and weight matrices
+"""
+
+
+import numpy as np
+
+
+def policy(matrix, weight):
+    """
+    Function that computes policy with a weight of a matrix
+
+    Args:
+        matrix [numpy.ndarray]: the matrix to compute policy from
+        weight [numpy.ndarray]: the weights applied to the matrix
+
+    Returns:
+        the policy
+    """
+    dot_product = matrix.dot(weight)
+    exp = np.exp(dot_product)
+    return exp / np.sum(exp)
+
+
+def policy_gradient(state, weight):
+    """
+    Function that computes the Monte Carlo policy gradient based on
+    the policy calculated from the above policy() function
+
+    Args:
+        state [numpy.ndarray]:
+            matrix representing the current observation of the environment
+        weight [numpy.ndarray]:
+            matrix of random weight
+
+    Returns:
+        the action and the gradient
+    """
+    my_policy = policy(state, weight)
+    action = np.random.choice(len(my_policy[0]), p=my_policy[0])
+    s = my_policy.reshape(-1, 1)
+    softmax = (np.diagflat(s) - np.dot(s, s.T))[action, :]
+    dlog = softmax / my_policy[0, action]
+    gradient = state.T.dot(dlog[None, :])
+    return action, gradient
