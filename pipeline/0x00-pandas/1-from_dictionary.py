187a625d7472ae6e262bb1ab7c996790756f6fd0
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-11-23 09:30:59 +0200

    Add Task 1

--- pipeline/0x00-pandas/1-from_dictionary.py
+++ pipeline/0x00-pandas/1-from_dictionary.py
@@ -1,1 +1,26 @@
-# 0x00-pandas
+#!/usr/bin/env python3
+"""
+1-from_dictionary.py
+Module that defines a function called from_dictionary
+"""
+
+import pandas as pd
+
+
+def from_dictionary():
+    """
+    Function that creates a Pandas DataFrame from a dictionary
+
+    Returns:
+        the newly created pd.DataFrame
+    """
+    df = pd.DataFrame(
+        {
+            "First": [0.0, 0.5, 1.0, 1.5],
+            "Second": ["one", "two", "three", "four"]
+        },
+        index=list("ABCD"))
+    return df
+
+
+df = from_dictionary()
