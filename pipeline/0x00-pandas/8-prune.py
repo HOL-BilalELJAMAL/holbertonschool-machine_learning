5c2ddb220eb201b769b177fc4d43c64823622780
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-11-23 10:15:52 +0200

    Add Task 8

--- pipeline/0x00-pandas/8-prune.py
+++ pipeline/0x00-pandas/8-prune.py
@@ -1,1 +1,12 @@
-# 0x00-pandas
+#!/usr/bin/env python3
+"""
+8-prune.py
+Module that removes the entries in the DataFrame where Close is NaN
+"""
+
+import pandas as pd
+from_file = __import__('2-from_file').from_file
+
+df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+df = df.dropna(subset=['Close'])
+print(df.head())
