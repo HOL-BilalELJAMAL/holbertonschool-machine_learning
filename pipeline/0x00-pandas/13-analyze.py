42df24ba33c261d825762c6913cd9525b1df70d2
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-11-23 10:21:39 +0200

    Add Task 13

--- pipeline/0x00-pandas/13-analyze.py
+++ pipeline/0x00-pandas/13-analyze.py
@@ -1,1 +1,13 @@
-# 0x00-pandas
+#!/usr/bin/env python3
+"""
+13-analyze.py
+Module that calculates descriptive statistics for all columns
+except Timestamp
+"""
+
+import pandas as pd
+from_file = __import__('2-from_file').from_file
+
+df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+stats = df.drop(columns=['Timestamp']).describe()
+print(stats)
