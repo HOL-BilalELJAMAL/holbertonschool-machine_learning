7fdf94ab1a48551395b6e4cd4533460b09681752
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-11-23 09:29:15 +0200

    Add Task 0

--- pipeline/0x00-pandas/0-from_numpy.py
+++ pipeline/0x00-pandas/0-from_numpy.py
@@ -1,1 +1,29 @@
-# 0x00-pandas
+#!/usr/bin/env python3
+"""
+0-from_numpy.py
+Module that defines a function called from_numpy
+"""
+
+import pandas as pd
+
+
+def from_numpy(array):
+    """
+    Function that creates a Pandas DataFrame from a numpy.ndarray
+
+    Args:
+        array [numpy.ndarray]: array to create pd.DataFrame from
+        columns of the DataFrame should be labeled in alphabetical
+        order and capitalized (there will not be more than 26 columns)
+
+    Returns:
+        The newly created pd.DataFrame
+    """
+    alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I",
+                "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+                "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    column_labels = []
+    for i in range(len(array[0])):
+        column_labels.append(alphabet[i])
+    df = pd.DataFrame(array, columns=column_labels)
+    return df
