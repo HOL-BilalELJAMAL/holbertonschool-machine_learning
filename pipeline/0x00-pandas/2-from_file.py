d64f26b60429545b32d40a38740b01382d32e89b
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-11-23 09:32:27 +0200

    Add Task 2

--- pipeline/0x00-pandas/2-from_file.py
+++ pipeline/0x00-pandas/2-from_file.py
@@ -1,1 +1,22 @@
-# 0x00-pandas
+#!/usr/bin/env python3
+"""
+2-from_file.py
+Module that defines a function called from_file
+"""
+
+import pandas as pd
+
+
+def from_file(filename, delimiter):
+    """
+    Function that loads data from a file as a Pandas DataFrame
+
+    Args:
+        filename [str]: file to load the data from
+        delimiter [str]: the column separator
+
+    Returns:
+        The newly created pd.DataFrame
+    """
+    df = pd.read_csv(filename, delimiter=delimiter)
+    return df
