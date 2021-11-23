006f02edc9baf85401faac09dd34bee955f9c7b0
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-11-23 10:14:20 +0200

    Add Task 6

--- pipeline/0x00-pandas/6-flip_switch.py
+++ pipeline/0x00-pandas/6-flip_switch.py
@@ -1,1 +1,13 @@
-# 0x00-pandas
+#!/usr/bin/env python3
+"""
+6-flip_switch.py
+Module that transposes the rows and columns and then sorts the data
+in reverse chronological order
+"""
+
+import pandas as pd
+from_file = __import__('2-from_file').from_file
+
+df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+df = df.sort_values(by='Timestamp', ascending=False).T
+print(df.tail(8))
