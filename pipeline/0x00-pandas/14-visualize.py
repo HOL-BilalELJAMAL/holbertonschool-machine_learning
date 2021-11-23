3b149d495c3e9e15ce524d0b9686cc447c412185
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-11-23 10:23:09 +0200

    Add Task 14

--- pipeline/0x00-pandas/14-visualize.py
+++ pipeline/0x00-pandas/14-visualize.py
@@ -1,1 +1,33 @@
-# 0x00-pandas
+#!/usr/bin/env python3
+"""
+14-visualize.py
+Module that updates the script to visualize the DataFrame
+"""
+
+from datetime import date
+import matplotlib.pyplot as plt
+import pandas as pd
+from_file = __import__('2-from_file').from_file
+
+df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+df = df.drop(columns=['Weighted_Price'])
+df = df.rename(columns={'Timestamp': 'Date'})
+df['Date'] = pd.to_datetime(df['Date'], unit='s')
+df['Date'] = df['Date'].dt.to_period('d')
+df = df.loc[df['Date'] >= "2017-01-01"]
+df = df.set_index('Date')
+df['Close'].fillna(method='pad', inplace=True)
+df['High'].fillna(df.Close, inplace=True)
+df['Low'].fillna(df.Close, inplace=True)
+df['Open'].fillna(df.Close, inplace=True)
+df['Volume_(BTC)'].fillna(value=0, inplace=True)
+df['Volume_(Currency)'].fillna(value=0, inplace=True)
+df_plot = pd.DataFrame()
+df_plot['High'] = df['High'].resample('d').max()
+df_plot['Low'] = df['Low'].resample('d').min()
+df_plot['Open'] = df['Open'].resample('d').mean()
+df_plot['Close'] = df['Close'].resample('d').mean()
+df_plot['Volume_(BTC)'] = df['Volume_(BTC)'].resample('d').sum()
+df_plot['Volume_(Currency)'] = df['Volume_(Currency)'].resample('d').sum()
+df_plot.plot()
+plt.show()
