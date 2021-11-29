f76748248ac65073d9945669aad6e7838772f59a
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-11-29 15:51:25 +0200

    Add Task 0

--- pipeline/0x01-apis/0-passengers.py
+++ pipeline/0x01-apis/0-passengers.py
@@ -1,1 +1,40 @@
-# 0x01-apis
+#!/usr/bin/env python3
+"""
+0-passengers.py
+Module that defines a function called availableShips
+"""
+
+import requests
+
+
+def availableShips(passengerCount):
+    """
+    Function that uses the Star Wars API to return the list of ships that
+    can hold passengerCount number of passengers
+
+    Args:
+        passengerCount [int]: the number of passenger the ship must be able to
+        carry
+
+    Returns:
+        [list]: all ships that can hold that many passengers
+    """
+    if type(passengerCount) is not int:
+        raise TypeError(
+            "passengerCount must be a positive number of passengers")
+    if passengerCount < 0:
+        raise ValueError(
+            "passengerCount must be a positive number of passengers")
+    url = "https://swapi-api.hbtn.io/api/starships/?format=json"
+    ships = []
+    while url:
+        results = requests.get(url).json()
+        ships += results.get('results')
+        url = results.get('next')
+    shipsList = []
+    for ship in ships:
+        passengers = ship.get('passengers').replace(",", "")
+        if passengers != "n/a" and passengers != "unknown":
+            if int(passengers) >= passengerCount:
+                shipsList.append(ship.get('name'))
+    return shipsList
