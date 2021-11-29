5ef65008346a310d75dfb4f6b64e0b55a07f7d03
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-11-29 15:52:48 +0200

    Add Task 1

--- pipeline/0x01-apis/1-sentience.py
+++ pipeline/0x01-apis/1-sentience.py
@@ -1,1 +1,32 @@
-# 0x01-apis
+#!/usr/bin/env python3
+"""
+1-sentience.py
+Module that defines a function called sentientPlanets
+"""
+
+import requests
+
+
+def sentientPlanets():
+    """
+    Function that uses the Star Wars API to return the list of home planets
+    for all sentient species
+
+    Returns:
+        [list]: home planets of sentient species
+    """
+    url = "https://swapi-api.hbtn.io/api/species/?format=json"
+    speciesList = []
+    while url:
+        results = requests.get(url).json()
+        speciesList += results.get('results')
+        url = results.get('next')
+    homePlanets = []
+    for species in speciesList:
+        if species.get('designation') == 'sentient' or \
+           species.get('classification') == 'sentient':
+            url = species.get('homeworld')
+            if url:
+                planet = requests.get(url).json()
+                homePlanets.append(planet.get('name'))
+    return homePlanets
