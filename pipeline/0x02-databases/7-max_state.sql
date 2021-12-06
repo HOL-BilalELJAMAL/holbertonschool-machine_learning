23fb449d6c025b0fd5eabc3876cc6752d3751e5d
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-12-06 11:22:21 +0200

    Add Task 7

--- pipeline/0x02-databases/7-max_state.sql
+++ pipeline/0x02-databases/7-max_state.sql
@@ -1,1 +1,4 @@
-# 0x02-databases
+-- Displays the max temperatures of each state, ordered by state name
+-- uses hbtn_0c_0 database
+-- database name will be passed as an argument of the mysql command
+SELECT `state`, MAX(value) AS max_temp FROM temperatures GROUP BY `state` ORDER BY `state`;
