d10ea64bb8a059d9a268600648712abd0cc21e9d
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-12-06 11:50:25 +0200

    Add Task 40

--- pipeline/0x02-databases/105-students.py
+++ pipeline/0x02-databases/105-students.py
@@ -1,1 +1,31 @@
-# 0x02-databases
+#!/usr/bin/env python3
+"""
+105-students.py
+Module that defines a function called top_students
+"""
+
+
+def top_students(mongo_collection):
+    """
+    Function that finds list of all schools sorted by average score
+
+    Args:
+        mongo_collection [pymongo]: the MongoDB collection to use top must be
+        ordered average score must be part of each item returns
+        with key=averageScore
+
+    Returns:
+        list of all students sorted by average score
+    """
+    students = []
+    documents = mongo_collection.find()
+    for student in documents:
+        total_score = 0
+        topics = student["topics"]
+        for project in topics:
+            total_score += project["score"]
+        average_score = total_score / len(topics)
+        student["averageScore"] = average_score
+        students.append(student)
+    students = sorted(students, key=lambda i: i["averageScore"], reverse=True)
+    return students
