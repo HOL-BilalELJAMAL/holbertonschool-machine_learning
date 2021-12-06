5cd1e8d01344eb4656eb1db8f5587b56ecd1b94c
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-12-06 11:20:46 +0200

    Add Task 5

--- pipeline/0x02-databases/5-average.sql
+++ pipeline/0x02-databases/5-average.sql
@@ -1,1 +1,4 @@
-# 0x02-databases
+-- Computes the average of all records in second_table in MySQL server
+-- resulting column name should be 'average'
+-- database name will be passed as an argument of the mysql command
+SELECT AVG(`score`) 'average' FROM `second_table`;
