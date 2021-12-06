c55905eb5b7789651daf0fa6ee406de705b09dac
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-12-06 11:38:12 +0200

    Add Task 30

--- pipeline/0x02-databases/30-all.py
+++ pipeline/0x02-databases/30-all.py
@@ -1,1 +1,22 @@
-# 0x02-databases
+#!/usr/bin/env python3
+"""
+30-all.py
+Module that defines a function called list_all
+"""
+
+
+def list_all(mongo_collection):
+    """
+    Function that lists all documents in given MongoDB collection
+
+    Args:
+        mongo_collection: the collection to use
+
+    Returns:
+        list of all documents or 0 if no documents found
+    """
+    all_docs = []
+    collection = mongo_collection.find()
+    for document in collection:
+        all_docs.append(document)
+    return all_docs
