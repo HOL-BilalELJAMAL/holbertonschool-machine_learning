a1d789b515d021b9107fde861ef1fde24d4528c3
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-12-06 11:41:18 +0200

    Add Task 32

--- pipeline/0x02-databases/32-update_topics.py
+++ pipeline/0x02-databases/32-update_topics.py
@@ -1,1 +1,21 @@
-# 0x02-databases
+#!/usr/bin/env python3
+"""
+32-update_topics.py
+Module that defines a function called update_topics
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Function that changes all topics of a school document based on the name
+
+    Args:
+        mongo_collection [pymongo]:
+            the MongoDB collection to use
+        name [string]:
+            the school name to update
+        topics [list of strings]:
+            list of topics approached in the school
+    """
+    mongo_collection.update_many({'name': name},
+                                 {'$set': {'topics': topics}})
