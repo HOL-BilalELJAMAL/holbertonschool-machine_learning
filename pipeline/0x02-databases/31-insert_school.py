fadcdfb17a41dc5918213090abc4cd8ad52bc0cb
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-12-06 11:39:50 +0200

    Add Task 31

--- pipeline/0x02-databases/31-insert_school.py
+++ pipeline/0x02-databases/31-insert_school.py
@@ -1,1 +1,19 @@
-# 0x02-databases
+#!/usr/bin/env python3
+"""
+31-insert_school.py
+Module that defines a function called insert_school
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Function that inserts a new document in a MongoDB collection
+
+    Args:
+        kwargs: the new document to add
+
+    Retruns:
+        the new _id
+    """
+    document = mongo_collection.insert_one(kwargs)
+    return document.inserted_id
