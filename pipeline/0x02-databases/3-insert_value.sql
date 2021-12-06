07f3067bf5b3a1ca25852af967ad1470c856d175
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-12-06 11:19:15 +0200

    Add Task 3

--- pipeline/0x02-databases/3-insert_value.sql
+++ pipeline/0x02-databases/3-insert_value.sql
@@ -1,1 +1,4 @@
-# 0x02-databases
+-- Inserts a new row in the table called first_table in MySQL server
+-- new row: id = 89 and name = Holberton School
+-- database name will be passed as an argument of the mysql command
+INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');
