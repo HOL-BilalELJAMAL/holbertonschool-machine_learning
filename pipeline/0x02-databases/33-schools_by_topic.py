2135409ddf89b24a2039b3a21c91d2bc2d7847a6
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-12-06 11:42:36 +0200

    Add Task 33

--- pipeline/0x02-databases/33-schools_by_topic.py
+++ pipeline/0x02-databases/33-schools_by_topic.py
@@ -1,1 +1,25 @@
-# 0x02-databases
+#!/usr/bin/env python3
+"""
+33-schools_by_topic.py
+Module that defines a function called schools_by_topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Function that finds list of all schools with a specific topic
+
+    Args:
+        mongo_collection [pymongo]:
+            the MongoDB collection to use
+        topic [string]:
+            the topic to search for
+
+    Returns:
+        list of schools with the given topic
+    """
+    schools = []
+    documents = mongo_collection.find({'topics': {'$all': [topic]}})
+    for doc in documents:
+        schools.append(doc)
+    return schools
