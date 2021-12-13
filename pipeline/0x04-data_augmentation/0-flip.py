d8e1aafc83bf3a9de9f88ff229b2f321627c13c4
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-12-13 12:48:00 +0200

    Add Task 0

--- pipeline/0x04-data_augmentation/0-flip.py
+++ pipeline/0x04-data_augmentation/0-flip.py
@@ -1,1 +1,21 @@
-# 0x04-data_augmentation
+#!/usr/bin/env python3
+"""
+0-flip.py
+Module that defines a function called flip_image
+"""
+
+import tensorflow as tf
+
+
+def flip_image(image):
+    """
+    Function that flips an image horizontally
+
+    Args:
+        image [3D td.Tensor]:
+            contains the image to flip
+
+    Returns:
+        The flipped image
+    """
+    return tf.image.flip_left_right(image)
