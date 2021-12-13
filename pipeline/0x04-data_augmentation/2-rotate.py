daa17ea5bc9cec9c88672daad4e66383dcc26d31
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-12-13 12:50:16 +0200

    Add Task 2

--- pipeline/0x04-data_augmentation/2-rotate.py
+++ pipeline/0x04-data_augmentation/2-rotate.py
@@ -1,1 +1,21 @@
-# 0x04-data_augmentation
+#!/usr/bin/env python3
+"""
+2-rotate.py
+Module that defines a function called rotate_image
+"""
+
+import tensorflow as tf
+
+
+def rotate_image(image):
+    """
+    Function that rotates an image 90 degrees counter-clockwise
+
+    Args:
+        image [3D td.Tensor]:
+            contains the image to rotate
+
+    Returns:
+        the rotated image
+    """
+    return tf.image.rot90(image)
