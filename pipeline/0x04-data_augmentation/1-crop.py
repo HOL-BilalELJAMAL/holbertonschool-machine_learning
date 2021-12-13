2f1334cb896844db06a3bc87b453965705ac7a01
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-12-13 12:49:09 +0200

    Add Task 1

--- pipeline/0x04-data_augmentation/1-crop.py
+++ pipeline/0x04-data_augmentation/1-crop.py
@@ -1,1 +1,23 @@
-# 0x04-data_augmentation
+#!/usr/bin/env python3
+"""
+1-crop.py
+Module that defines a function called crop_image
+"""
+
+import tensorflow as tf
+
+
+def crop_image(image, size):
+    """
+    Function that performs a random crop of an image
+
+    Args:
+        image [3D td.Tensor]:
+            contains the image to crop
+        size [tuple]:
+            contains the size of the crop
+
+    Returns:
+        the cropped image
+    """
+    return tf.image.random_crop(image, crop_size=size)
