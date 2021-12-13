a31ff18ac6d00c5b1fc1e0281ab4c7dfe4120f3e
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-12-13 12:57:44 +0200

    Add Task 7

--- pipeline/0x04-data_augmentation/100-pca.py
+++ pipeline/0x04-data_augmentation/100-pca.py
@@ -1,1 +1,25 @@
-# 0x04-data_augmentation
+#!/usr/bin/env python3
+"""
+100-pca.py
+Module that defines a function called pca_color
+"""
+
+import tensorflow as tf
+
+
+def pca_color(image, alphas):
+    """
+    Function that performs PCA color augmentation on an image
+
+    Args:
+        image [3D td.Tensor]:
+            contains the image to change
+        alphas [tuple of length 3]:
+            contains the amount that each channel should change
+
+    Returns:
+        The augmented image
+    """
+    image_array = tf.keras.preprocessing.image.img_to_array(image)
+    img = image_array.reshape(-1, 3).astype(float)
+    return img
