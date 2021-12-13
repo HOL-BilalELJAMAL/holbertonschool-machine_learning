4968c4b0fe6e0091d81dcf55d7c9efef94d11be3
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-12-13 12:55:26 +0200

    Add Task 5

--- pipeline/0x04-data_augmentation/5-hue.py
+++ pipeline/0x04-data_augmentation/5-hue.py
@@ -1,1 +1,23 @@
-# 0x04-data_augmentation
+#!/usr/bin/env python3
+"""
+5-hue.py
+Module that defines a function called change_hue
+"""
+
+import tensorflow as tf
+
+
+def change_hue(image, delta):
+    """
+    Function that changes the hue of an image
+
+    Args:
+        image [3D td.Tensor]:
+            contains the image to change
+        delta [float]:
+            the amount the hue should change
+
+    Returns:
+        The altered image
+    """
+    return tf.image.adjust_hue(image, delta)
