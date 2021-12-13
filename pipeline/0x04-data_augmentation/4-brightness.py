8e5f4f730d233804af42d0646fc24576ec11e5eb
Bilal EL JAMAL <73889590+HOL-BilalELJAMAL@users.noreply.github.com>
2021-12-13 12:54:01 +0200

    Add Task 4

--- pipeline/0x04-data_augmentation/4-brightness.py
+++ pipeline/0x04-data_augmentation/4-brightness.py
@@ -1,1 +1,23 @@
-# 0x04-data_augmentation
+#!/usr/bin/env python3
+"""
+4-brightness.py
+Module that defines a function called change_brightness
+"""
+
+import tensorflow as tf
+
+
+def change_brightness(image, max_delta):
+    """
+    Function that randomly changes the brightness of an image
+
+    Args:
+        image [3D td.Tensor]:
+            contains the image to change
+        max_delta [float]:
+            maximum amount the image should be brightened (or darkened)
+
+    Returns:
+        the altered image
+    """
+    return tf.image.random_brightness(image, max_delta)
